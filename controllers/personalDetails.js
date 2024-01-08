247e003cf94450af2150a14872b4878cb4cfb6a5
Ashwinigadad <ashwinigadad3@gmail.com>
2024-01-08 19:14:19 +0530

    initial

---
+++ controllers/personalDetails.js
@@ -0,0 +1,37 @@
+const mongoose=require("../models/login");
+const logIn=mongoose.model("loginDetails");
+const bcrypt=require("bcrypt");
+const token=require("jsonwebtoken");
+const login=async (req, res) => {
+    try {
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  
+        // User not found, create a new entry
+        const loginEntry = new logIn({
+          email: req.body.email,
+          password:hashedPassword,
+
+        });
+        token.sign({loginEntry},"secretKey",{expiresIn:'300s'},(err,tok)=>{
+            res.json({
+                tok
+            })
+        })
+        
+        await loginEntry.save(); 
+
+      }
+
+
+    catch (err) {
+      console.error("Error handling entry/exit:", err.message);
+      res.status(500).json({ error: "Internal Server Error" });
+
+
+    }
+  }
+
+  module.exports=login;
