b9b1a17ac5a095ad52cb5396b377ea0e84b902ed
Ashwinigadad <ashwinigadad3@gmail.com>
2024-01-29 18:28:03 +0530

    modified

--- route/routes.js
+++ route/routes.js
@@ -5,5 +5,5 @@ route.post("/api/signUp",require("../controllers/signup"));
  route.post("/api/login", require("../controllers/login"));
  route.post("/api/entryexit",require("../controllers/libraryEntryExit"));
  route.post("/api/personalDetails",require("../controllers/personalDetails"));
-
+ 
   module.exports=route;
